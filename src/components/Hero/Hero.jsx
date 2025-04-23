575bcc9720a36f56ed869a685f40409d0555109d
Aliasger <al.aliasger@gmail.com>
2025-04-23 20:24:20 +0530

    change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -43,11 +43,11 @@ const Hero = ({handleOrderPopup}) => {
     <div>
         <div className='relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200'>
             <div className='h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8]'></div>
-            <div className='container mx-auto sm:mx-2 pb-8 sm:pb-0'>
+            <div className='container mx-auto pb-8 sm:pb-0'>
                 <Slider {...settings}>
                     {ImageList.map((data)=>(
                         <div>
-                            <div className='grid grid-cols-1 sm:grid-cols-2 ml-16 sm:ml-8 lg:ml-32'>
+                            <div className='grid grid-cols-1 sm:grid-cols-2 md:ml-16 sm:ml-8 lg:ml-32'>
                                 <div className='flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10'>
                                     <h1 
                                     data-aos="zoom-out"
